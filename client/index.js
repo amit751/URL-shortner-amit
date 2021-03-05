cadc58024e32ed90173a90fc596c1b0338c79fe2
amit <amitoosh.mor@gmail.com>
2021-03-05 20:26:17 +0200

    usinc app.get is working

--- client/index.js
+++ client/index.js
@@ -2,32 +2,32 @@
 
 const button = document.getElementById("button");
 const input = document.getElementById("input");
-button.addEventListener("click" , ()=>{
-    if(input.value){
+// button.addEventListener("click" , ()=>{
+//     if(input.value){
 
-        const data = { url: input.value};
-        fetch("http://localhost:3000" , {
-            method: "POST" ,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then((response)=>{
-            response.json()
-        }).then((result)=>{
-            input.value= "";
-            console.log(result);
+//         const data = { url: input.value};
+//         fetch("http://localhost:3000" , {
+//             method: "POST" ,
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         }).then((response)=>{
+//             response.json()
+//         }).then((result)=>{
+//             input.value= "";
+//             console.log(result);
     
-        });
+//         });
 
-        // const data = { url: input.value};
-        // const options = {
-        //     method: "POST",
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(data)
-        // }
-        // fetch("http://localhost:3000", options);
-    }
-});
+//         // const data = { url: input.value};
+//         // const options = {
+//         //     method: "POST",
+//         //     headers: {
+//         //         'Content-Type': 'application/json'
+//         //     },
+//         //     body: JSON.stringify(data)
+//         // }
+//         // fetch("http://localhost:3000", options);
+//     }
+// });
--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const express = require("express");
 const fetch = require('node-fetch');
 const fs = require("fs");
 const cors = require("cors");
-const { resolve } = require("path");
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-// app.use(express.static('DB/urls-bin'));
+app.use(express.static('DB/urls-bin'));
 
 app.use(cors()); 
 
@@ -16,66 +15,6 @@ app.listen( 3000 , ()=>{
     console.log("listen at 3000")
 });
 app.use(express.json());
-
-
-
-
-
-app.post("/" , (req , res)=>{
-    const url = req.body.url;
-    console.log(url);
-    const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
-    if(url in bin){
-        
-        res.send(bin[url].shorturl).status(200);
-        return;
-    }
-    fetch(url).then((response)=>{
-        if(response.ok){
-            console.log(response);
-            let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
-            console.log(newID);
-            bin[url] = {
-                creationDate : new Date() ,
-                redirectCount :  0 ,
-                originalUrl : url,
-                shorturl : `http://localhost:3000/${newID}` ,
-                id : newID
-            };
-            ++newID;
-            console.log(newID);
-            fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
-            fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
-            res.status(200).json( bin[url]);
-            
-        }else{
-            console.log(response);
-          res.send("invalid website");  
-        }
-    });
-});
-
-app.get ("/:id" , (request , response)=>{
-    console.log(request.params);
-    // let params = request.params
-    const { id } = request.params;
-    console.log(id);
-    const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
-    let originalUrl = false;
-    console.log(bin);
-   
-    for (const prop in bin) {
-      const urlPROP = bin[prop]
-      if(urlPROP.id === Number(id) ){
-        originalUrl = urlPROP.originalUrl;
-      }
-    }
-    if(!originalUrl){
-        throw new Error("not found");
-    }
-    response.redirect(301 , originalUrl);
-    
-});
 
 class Db{
     constructor(){
@@ -116,9 +55,100 @@ class Db{
 }
 dataBase = new Db;
 
-dataBase.writeFile(`./DB/urls-bin/test.json`, JSON.stringify({data: "test"} , null, 4)).then((result)=>{
-    if (result){
-        dataBase.readFile(`./DB/urls-bin/test.json`).then((data)=>{console.log(data);});
-    }
+app.get ("/:id" , (request , response)=>{
+    console.log( "inside get");
+    // console.log(request.params);
+    // let params = request.params
+    const { id } = request.params;
+    dataBase.readFile(`./DB/urls-bin/short-urls.json`)
+    .then((data)=>{
+        const bin = JSON.parse(data);
+        let originalUrl = false;
+        for (const prop in bin) {
+            const urlPROP = bin[prop]
+            if(urlPROP.id === Number(id) ){
+              originalUrl = urlPROP.originalUrl;
+            }
+        }if(!originalUrl){
+            throw new Error("not found-there is not such url");
+        }
+        response.redirect(303 , originalUrl); 
+        return;
+    })
+    .catch((err)=>{
+        res.json(err);
+        return;
+    })
+    // console.log(id);
+    // const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
+    // let originalUrl = false;
+    // // console.log(bin);
+   
+    // for (const prop in bin) {
+    //   const urlPROP = bin[prop]
+    //   if(urlPROP.id === Number(id) ){
+    //     originalUrl = urlPROP.originalUrl;
+    //   }
+    // }
+    // console.log(":origin url" , originalUrl);
+    // if(!originalUrl){
+    //     throw new Error("not found");
+        
+    // }
+    // response.redirect(303 , originalUrl);
+    return;
+    
 });
+
+
+
+app.post("/urlshorts" , (req , res)=>{
+    const url = req.body.url;
+    console.log("nowwwww" , url);
+    const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
+    if(url in bin){
+        console.log("short exist");
+        res.send(bin[url].shorturl).status(200);
+        return;
+    }if(url === false){
+        res.json({url : `${url} isnot valid` }).status(200);
+    }
+   return fetch(url).then((response)=>{
+        if(response.ok){
+            // console.log(response);
+            let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
+            // console.log(newID);
+            bin[url] = {
+                creationDate : new Date() ,
+                redirectCount :  0 ,
+                originalUrl : url,
+                shorturl : `http://localhost:3000/${newID}` ,
+                id : newID
+            };
+            ++newID;
+            // console.log(newID);
+            fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
+            fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
+            res.status(200).json( bin[url]);
+            console.log("end post");
+            return;
+            
+        }else{
+            // console.log(response);
+          res.send("invalid website");
+          return;  
+        }
+    });
+    // return;
+});
+console.log("between post & get");
+
+
+
+
+// dataBase.writeFile(`./DB/urls-bin/test.json`, JSON.stringify({data: "test"} , null, 4)).then((result)=>{
+//     if (result){
+//         dataBase.readFile(`./DB/urls-bin/test.json`).then((data)=>{console.log(data);});
+//     }
+// });
 

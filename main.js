8f7c53328f9401f5168013c9196f3823f05c5682
amit <amitoosh.mor@gmail.com>
2021-03-05 23:04:12 +0200

    async post method works

--- main.js
+++ main.js
@@ -99,7 +99,10 @@ app.get ("/:id" , (request , response)=>{
     return;
     
 });
-function readFileSUCSESS(counterData , bin , url){
+
+
+function readFileSUCSESS(counterData , bin , url , res){
+   console.log("here res" );
     let newID = JSON.parse(counterData).counter; 
     bin[url] = {
         creationDate : new Date() ,
@@ -109,22 +112,32 @@ function readFileSUCSESS(counterData , bin , url){
         id : newID
     };
     ++newID;
-    Promise.all([ dataBase.writeFile(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4)) , 
-     dataBase.writeFile(`./DB/id-genrator.json` , JSON.stringify({counter : newID }))])
-    .then((values)=>{
-        return res.status(200).json( bin[url]); 
+    dataBase.writeFile(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4))
+    .then((resultfromurl)=>{
+        dataBase.writeFile(`./DB/id-genrator.json` , JSON.stringify({counter : newID }))
+        .then((resultfromcounter)=>{
+            return res.json( bin[url]); //status undifined 
+        }) 
     }).catch((err)=>{
         console.log("db.writefile faild could not save to db");
         return res.json( err); 
     }); 
+    // Promise.all([ dataBase.writeFile(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4)) , 
+    //  dataBase.writeFile(`./DB/id-genrator.json` , JSON.stringify({counter : newID }))])
+    // .then((values)=>{
+    //     return res.status(200).json( bin[url]); 
+    // }).catch((err)=>{
+    //     console.log("db.writefile faild could not save to db");
+    //     return res.json( err); 
+    // }); 
     
 }
-function readFileFail(err){
+function readFileFail(err , res){
     return res.send(err);
     
 }
 
-function onFullfild(data ,url){
+function onFullfild(data ,url , res){
     const bin = JSON.parse(data);
     if(url in bin){
         res.send(bin[url].shorturl).status(200);
@@ -132,11 +145,12 @@ function onFullfild(data ,url){
     }if(url === false){
         res.json({url : `${url} isnot valid` }).status(200);
     }
-    return fetch(url).then((response)=>{
+    return fetch(url)
+    .then((response)=>{
         if(response.ok){
             dataBase.readFile(`./DB/id-genrator.json`)
             .then((counterData)=>{
-                readFileSUCSESS(counterData , bin , url);
+                readFileSUCSESS(counterData , bin , url , res);
             } , (err)=>{
                 readFileFail(err);
             });
@@ -146,6 +160,10 @@ function onFullfild(data ,url){
           res.send("invalid website");
           return;  
         }
+    }).catch((e)=>{
+        console.log(`couldnt fetch to${url}`);
+        return res.send("invalid website");
+        
     });
 }
 app.post("/urlshorts" , (req , res)=>{
@@ -153,7 +171,7 @@ app.post("/urlshorts" , (req , res)=>{
     console.log("nowwwww" , url);
     dataBase.readFile(`./DB/urls-bin/short-urls.json`)
     .then((data)=>{
-        return onFullfild(data ,url);
+        return onFullfild(data ,url ,res);
     })
     .catch((err)=>{
         console.log(err , "in post");

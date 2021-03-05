c952ad36e235afad02644931103f90d74e3f3647
amit <amitoosh.mor@gmail.com>
2021-03-05 12:46:19 +0200

    using form with the backend

--- main.js
+++ main.js
@@ -1,36 +1,54 @@
-const { response } = require("express");
+
 const express = require("express");
-// const { request } = require("http");
-const fs = require("fs"); 
+const fetch = require('node-fetch');
+const fs = require("fs");
+const cors = require("cors");
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+// app.use(express.static('DB/urls-bin'));
+
+app.use(cors()); 
+
+
 app.listen( 3000 , ()=>{
     console.log("listen at 3000")
 });
 app.use(express.json());
 
 
-app.post("/" , (request , response)=>{
-    const url = request.body.url;
+app.post("/" , (req , res)=>{
+    const url = req.body.url;
     console.log(url);
     const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
     if(url in bin){
-        response.send(bin[url].shorturl).status(200);
+        
+        res.send(bin[url].shorturl).status(200);
         return;
     }
-    let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
-    console.log(newID);
-    bin[url] = {
-        creationDate : new Date() ,
-        redirectCount :  0 ,
-        originalUrl : url,
-        shorturl : `http://localhost:3000/${newID}` ,
-        id : newID
-    };
-    ++newID;
-    console.log(newID);
-    fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
-    fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
-    response.json( bin[url]).status(200);
+    fetch(url).then((response)=>{
+        if(response.ok){
+            console.log(response);
+            let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
+            console.log(newID);
+            bin[url] = {
+                creationDate : new Date() ,
+                redirectCount :  0 ,
+                originalUrl : url,
+                shorturl : `http://localhost:3000/${newID}` ,
+                id : newID
+            };
+            ++newID;
+            console.log(newID);
+            fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
+            fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
+            res.status(200).json( bin[url]);
+            
+        }else{
+            console.log(response);
+          res.send("invalid website");  
+        }
+    });
 });
 
 app.get ("/:id" , (request , response)=>{
@@ -43,7 +61,7 @@ app.get ("/:id" , (request , response)=>{
     console.log(bin);
    
     for (const prop in bin) {
-        const urlPROP = bin[prop]
+      const urlPROP = bin[prop]
       if(urlPROP.id === Number(id) ){
         originalUrl = urlPROP.originalUrl;
       }

a9291fa5b774e117c82b7d8604e5ef472d33d59f
amit <amitoosh.mor@gmail.com>
2021-03-05 21:53:59 +0200

    promis.all dont work

--- main.js
+++ main.js
@@ -99,49 +99,131 @@ app.get ("/:id" , (request , response)=>{
     return;
     
 });
+function readFileSUCSESS(counterData , bin , url){
+    let newID = JSON.parse(counterData).counter; 
+    bin[url] = {
+        creationDate : new Date() ,
+        redirectCount :  0 ,
+        originalUrl : url,
+        shorturl : `http://localhost:3000/${newID}` ,
+        id : newID
+    };
+    ++newID;
+    Promise.all([ dataBase.writeFile(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4)) , 
+     dataBase.writeFile(`./DB/id-genrator.json` , JSON.stringify({counter : newID }))])
+    .then((values)=>{
+        return res.status(200).json( bin[url]); 
+    }).catch((err)=>{
+        console.log("db.writefile faild could not save to db");
+        return res.json( err); 
+    }); 
+    
+}
+function readFileFail(err){
+    return res.send(err);
+    
+}
 
-
-
-app.post("/urlshorts" , (req , res)=>{
-    const url = req.body.url;
-    console.log("nowwwww" , url);
-    const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
+function onFullfild(data ,url){
+    const bin = JSON.parse(data);
     if(url in bin){
-        console.log("short exist");
         res.send(bin[url].shorturl).status(200);
         return;
     }if(url === false){
         res.json({url : `${url} isnot valid` }).status(200);
     }
-   return fetch(url).then((response)=>{
+    return fetch(url).then((response)=>{
         if(response.ok){
-            // console.log(response);
-            let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
-            // console.log(newID);
-            bin[url] = {
-                creationDate : new Date() ,
-                redirectCount :  0 ,
-                originalUrl : url,
-                shorturl : `http://localhost:3000/${newID}` ,
-                id : newID
-            };
-            ++newID;
-            // console.log(newID);
-            fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
-            fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
-            res.status(200).json( bin[url]);
-            console.log("end post");
+            dataBase.readFile(`./DB/id-genrator.json`)
+            .then((counterData)=>{
+                readFileSUCSESS(counterData , bin , url);
+            } , (err)=>{
+                readFileFail(err);
+            });
             return;
             
         }else{
-            // console.log(response);
           res.send("invalid website");
           return;  
         }
     });
-    // return;
+}
+app.post("/urlshorts" , (req , res)=>{
+    const url = req.body.url;
+    console.log("nowwwww" , url);
+    dataBase.readFile(`./DB/urls-bin/short-urls.json`)
+    .then((data)=>{
+        return onFullfild(data ,url);
+    })
+    .catch((err)=>{
+        console.log(err , "in post");
+        return res.send(err);
+    });
 });
-console.log("between post & get");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/urlshorts" , (req , res)=>{
+//     const url = req.body.url;
+//     console.log("nowwwww" , url);
+    
+//     const bin = JSON.parse(fs.readFileSync(`./DB/urls-bin/short-urls.json`));
+//     if(url in bin){
+//         console.log("short exist");
+//         res.send(bin[url].shorturl).status(200);
+//         return;
+//     }if(url === false){
+//         res.json({url : `${url} isnot valid` }).status(200);
+//     }
+//    return fetch(url).then((response)=>{
+//         if(response.ok){
+//             // console.log(response);
+//             let newID = JSON.parse(fs.readFileSync(`./DB/id-genrator.json`)).counter;
+//             // console.log(newID);
+//             bin[url] = {
+//                 creationDate : new Date() ,
+//                 redirectCount :  0 ,
+//                 originalUrl : url,
+//                 shorturl : `http://localhost:3000/${newID}` ,
+//                 id : newID
+//             };
+//             ++newID;
+//             // console.log(newID);
+//             fs.writeFileSync(`./DB/urls-bin/short-urls.json` , JSON.stringify(bin , null, 4));
+//             fs.writeFileSync(`./DB/id-genrator.json` , JSON.stringify({counter : newID }));
+//             res.status(200).json( bin[url]);
+//             console.log("end post");
+//             return;
+            
+//         }else{
+//             // console.log(response);
+//           res.send("invalid website");
+//           return;  
+//         }
+//     });
+    
+// });
+
 
 
 
